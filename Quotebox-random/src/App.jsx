93ecc69975b61503fd1cec6cce9238784857e681
Gabriel Mancilla Ducuara <98790615+gaboducuara@users.noreply.github.com>
2022-10-26 10:16:21 -0500

    Add files via upload

---
+++ Quotebox-random/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Quotes from "./dashboard/Quotes/Quotes.jsx";
+import './index.css'
+
+function App() {
+    return (
+        <div className="App">
+            <Quotes />
+        </div>
+    );
+}
+
+export default App;
